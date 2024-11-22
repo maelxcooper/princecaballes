83f56016197e6cf2414871b98f502c40650ed43f
Prince Caballes <prncaballes@proweaver.net>
2024-11-23 01:35:38 +0800

    Hero text sizing - mobile adjustments

--- src/components/sections/Hero.jsx
+++ src/components/sections/Hero.jsx
@@ -16,10 +16,10 @@ const Hero = () => {
         <div id='Home' className='h-screen'>
             <div className='max-w-[1000px] mt-[-96px] h-[84vh] w-full mx-auto text-center flex flex-col justify-center'>
                 <p className='introName md:text-[18px] text-[var(--priColor)] font-normal p-2'>Hello, My name is</p>
-                <h1 className='devName md:text-5xl sm:text-4xl text-3xl stroke-text font-bold font-montserrat md:py-1'>Prince Caballes.</h1>
+                <h1 className='devName md:text-5xl sm:text-4xl text-2xl stroke-text font-bold font-montserrat md:py-1'>Prince Caballes.</h1>
                 <div className='posTitle flex justify-center items-center flex-wrap md:py-4'>
-                    <p className='md:text-3xl-cm sm:text-4xl  text-3xl stroke-text font-montserrat font-bold'> I'm a</p>
-                    <ReactTyped className='md:text-3xl-cm sm:text-4xl text-3xl text-[var(--priColor)]  font-montserrat font-bold italic pl-2 ' strings={['Front End Developer', 'Seo', 'Virtual Assistant']} typeSpeed={140} backSpeed={90} loop />
+                    <p className='md:text-3xl-cm sm:text-4xl  text-2xl stroke-text font-montserrat font-bold'> I'm a</p>
+                    <ReactTyped className='md:text-3xl-cm sm:text-4xl text-2xl text-[var(--priColor)]  font-montserrat font-bold italic pl-2 ' strings={['Front End Developer', 'Seo', 'Virtual Assistant']} typeSpeed={140} backSpeed={90} loop />
                 </div>
             </div>
         </div>
