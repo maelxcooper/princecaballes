a21bab1f43fb1ee08e38dcd1f6db8f665b7d4388
Prince Caballes <pw.pncaballes@proweaver.net>
2025-03-11 04:19:51 +0800

    updated sidebar responsive

--- src/components/sections/Sidebar.jsx
+++ src/components/sections/Sidebar.jsx
@@ -37,7 +37,7 @@ const Sidebar = () => {
 
     return (
         <div className='relative'>
-            <div className='socialsLink hidden md:block fixed w-10 left-[2.5rem] bottom-0 text-[var(--lightest-slate)]'>
+            <div className='socialsLink hidden sm:block fixed w-10 left-[2.5rem] bottom-0 text-[var(--lightest-slate)]'>
                 <UlStyled className='flex flex-col items-center m-0 p-0'>
                     <li><a className='p-2.5 hover:translate-y-[-3px] hover:text-[var(--priColor)]' href="https://github.com/maelxcooper"><Github /></a></li>
                     <li><a className='p-2.5 hover:translate-y-[-3px] hover:text-[var(--priColor)]' href="https://www.instagram.com/maelxcooper/"><Instagram /></a></li>
@@ -47,7 +47,7 @@ const Sidebar = () => {
             </div>
 
 
-            <div className='hidden md:block fixed w-10 right-[1rem] bottom-0 z-10 text-[var(--lightest-slate)]'>
+            <div className='hidden sm:block fixed w-10 right-[1rem] bottom-0 z-10 text-[var(--lightest-slate)]'>
                 <StyledSidebar className='email__styledLink relative flex flex-col items-center'>
                     <a href="mailto:caballesprince3211@gmail.com">caballesprince3211@gmail.com</a>
                 </StyledSidebar>
